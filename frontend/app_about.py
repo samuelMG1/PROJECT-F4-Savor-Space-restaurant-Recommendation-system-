54de53a352963e652eac9ca326edf5ff31e9c5d1
samuel gathogo <111558517+samuelMG1@users.noreply.github.com>
2024-10-10 09:35:22 +0300

    Update

--- frontend/app_about.py
+++ frontend/app_about.py
@@ -31,9 +31,9 @@ def about_us():
         with tab1:
             with st.container(border=True):
                 st.header('Company History', divider=True)
-                st.write(" ''', a dynamic enterprise started in 2024. Founded by a group of passionate young minds, the company is committed to addressing global challenges through the innovative lens of culinary arts.")
-                st.write("Recognizing the power of food to connect, nourish, and inspire,  '''' is on a mission to redefine the culinary landscape. By harnessing the creativity and expertise cultivated during their Moringa education, the founders are developing innovative food solutions that cater to diverse tastes and dietary needs.")
-                st.write("From the heart of the classroom to the forefront of the food industry,  ''''' embodies the entrepreneurial drive of a new generation. The company's dedication to global problem-solving is evident in its commitment to sustainability, ethical sourcing, and community development. By combining culinary excellence with a strong social conscience,  GRACIOUS RESTAURANT aims to create a positive impact on both individuals and the planet.")
+                st.write(" SavorSpace, a dynamic enterprise started in 2024. Founded by a group of passionate young minds, the company is committed to addressing global challenges through the innovative lens of culinary arts.")
+                st.write("Recognizing the power of food to connect, nourish, and inspire,  SavorSpace is on a mission to redefine the culinary landscape. By harnessing the creativity and expertise cultivated during their Moringa education, the founders are developing innovative food solutions that cater to diverse tastes and dietary needs.")
+                st.write("From the heart of the classroom to the forefront of the food industry,  SavorSpace embodies the entrepreneurial drive of a new generation. The company's dedication to global problem-solving is evident in its commitment to sustainability, ethical sourcing, and community development. By combining culinary excellence with a strong social conscience,  GRACIOUS RESTAURANT aims to create a positive impact on both individuals and the planet.")
         with tab2:
             with st.container(border=True):
                 st.header("Mission Statement", divider=True)
